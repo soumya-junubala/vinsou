1e256c55eada35fdd2583f39c65e61ffe1b64f51
soumya-junubala <133960866+soumya-junubala@users.noreply.github.com>
2025-02-01 19:16:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.get("/", (req, res) => {
   res.send("Backend is running! 🚀");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
